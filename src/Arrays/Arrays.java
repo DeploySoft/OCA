d40ae9e90c492e2c7579d3ff8adb89b6b90e1888
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-07-30 20:01:08 -0500

    Incorrect ways to instance a array

--- src/Arrays/Arrays.java
+++ src/Arrays/Arrays.java
@@ -5,7 +5,13 @@ public class Arrays {
         short[] arr;
         arr = new short[2];
         short[] arr2 = new short[]{100, 100};
-        short arr3 [] = new short[2];
+        short arr3[] = new short[2];
+        //Wrong ways to instance a array
+        short[] badarr = {};
+        short[] badarr2 = new short[2]{100, 100};
+        short[2] badarr3;
+
+
     }
 
 }
