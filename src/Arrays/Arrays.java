9ab980925de2d595c18ed7bfd488bfc53bf70a8f
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-07-30 19:42:12 -0500

    Corrects ways to instance a array

---
+++ src/Arrays/Arrays.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        short[] arr;
+        arr = new short[2];
+        short[] arr2 = new short[]{100, 100};
+        short arr3 [] = new short[2];
+    }
+
+}
