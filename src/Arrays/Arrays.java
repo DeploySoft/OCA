75d0d28e9a8493b9252e3e3634fe46b41aa60683
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-07-30 20:05:12 -0500

    New ways

--- src/Arrays/Arrays.java
+++ src/Arrays/Arrays.java
@@ -2,14 +2,16 @@ package Arrays;
 
 public class Arrays {
     public static void main(String[] args) {
-        short[] arr;
-        arr = new short[2];
+        short[] arr = new short[2];
         short[] arr2 = new short[]{100, 100};
         short arr3[] = new short[2];
+        short arr4[][] = new short[2][2];
+        short arr5[][] = new short[2][];
         //Wrong ways to instance a array
         short[] badarr = {};
-        short[] badarr2 = new short[2]{100, 100};
-        short[2] badarr3;
+        short[] badarr2 = new short[2] {100, 100} ;
+        short[2]badarr3;
+        short badarr4[][] = new short[][ 2];
 
 
     }
