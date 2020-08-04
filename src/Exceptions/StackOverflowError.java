dcc267539c243302d2d8a4b4f2f27189ed660552
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-08-03 19:59:25 -0500

    StackOverflowError

---
+++ src/Exceptions/StackOverflowError.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class StackOverflowError {
+    public static void main(String[] args) {
+        try {
+            main(args);
+        } catch (Exception ex) {
+            System.out.println("CATCH-");
+        }
+        System.out.println("OUT");
+    }
+}
