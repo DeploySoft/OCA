ce1fc162039377f22a898979c2aac833f20e71fb
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-08-16 20:43:43 -0500

    Constructors

---
+++ src/JavaBasics/Constructors.java
@@ -0,0 +1,27 @@
+package JavaBasics;
+
+/**
+ * @author : J. Andrés Boyacá Silva
+ * @since : 8/16/2020, Sun
+ **/
+public class Constructors {
+
+}
+
+class Student {
+    String name;
+    int age;
+
+    /**
+     * between constructors should call with reserved word <b>this</b>
+     */
+    Student() {
+//        Student("James", 25);
+        this("James", 25);
+    }
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
