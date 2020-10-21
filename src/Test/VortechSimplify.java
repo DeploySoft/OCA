3f388511dbe424e3c82bb8a4798130548c45f5c1
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-10-21 14:44:33 -0500

    Strategy created with functional interface

--- src/Test/VortechSimplify.java
+++ src/Test/VortechSimplify.java
@@ -1,16 +1,19 @@
 package Test;
 
+import static Test.VortechSimplify.PaymentStrategy.CREDIT_CARD_FEE;
+import static Test.VortechSimplify.PaymentStrategy.SERVICE_CHARGE;
+
 /**
  * @author : J. Andres Boyaca (janbs)
  * @since : 21/10/20
  **/
 public class VortechSimplify {
 
-    public interface OldWayPaymentStrategy {
+    public interface OldWayPaymentStrategy { //Reduce
         double pay(double amount);
     }
 
-    public class CashPaymentStrategy implements OldWayPaymentStrategy {
+    public class CashPaymentStrategy implements OldWayPaymentStrategy { //Reduce
         @Override
         public double pay(double amount) {
             double serviceCharge = 5.00;
@@ -18,7 +21,7 @@ public class VortechSimplify {
         }
     }
 
-    public class CreditCardStrategy implements OldWayPaymentStrategy {
+    public class CreditCardStrategy implements OldWayPaymentStrategy { //Reduce
         @Override
         public double pay(double amount) {
             double serviceCharge = 5.00;
@@ -27,12 +30,22 @@ public class VortechSimplify {
         }
     }
 
+    @FunctionalInterface
     public interface PaymentStrategy {
+        double SERVICE_CHARGE = 5.00;
+        double CREDIT_CARD_FEE = 10.00;
 
-        //write here your solution
+        double pay(double amount);
+
     }
 
     public static void main(String[] args) {
+
+        PaymentStrategy creditCardStrategy = amount -> amount + SERVICE_CHARGE + CREDIT_CARD_FEE;
+        PaymentStrategy cashPaymentStrategy = amount -> amount + SERVICE_CHARGE;
+
+        System.out.println(creditCardStrategy.pay(5.00));
+        System.out.println(cashPaymentStrategy.pay(5.00));
 
     }
 }
