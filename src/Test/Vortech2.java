62fec4d5c0f57bc33bb79be3773db24fa1b90eaf
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-10-21 11:52:25 -0500

    Point 2 finished

--- src/Test/Vortech2.java
+++ src/Test/Vortech2.java
@@ -1,7 +1,9 @@
 package Test;
 
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author : J. Andres Boyaca (janbs)
@@ -10,11 +12,11 @@ import java.util.List;
 public class Vortech2 {
 
     public static void main(String[] args) {
-        List<Integer> lista = Arrays.asList(1, 2, 3, 4);
-        lista.forEach(integer -> {
-            if (integer % 2 == 0)
-                System.out.println(String.format("the number %s it's a prime number",integer));
-        });
+        List<Integer> result = new Vortech2().checkNumber(Arrays.asList(1, 2, 3, 4));
+        System.out.println(result);
+    }
 
+    private List<Integer> checkNumber(List<Integer> list) {
+        return list.stream().filter(number-> number % 2 == 0).collect(Collectors.toList());
     }
 }
